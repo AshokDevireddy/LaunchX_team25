031d1a0948f5040b36bd33f3302636bf34ef089a
Ashok Devireddy <53099335+AshokDevireddy@users.noreply.github.com>
2020-08-02 20:39:51 -0700

    Add files via upload

---
+++ Launchx Chrome Extension/content1.js
@@ -0,0 +1,23 @@
+
+img1 = document.createElement('img'); 
+img1.src =  'https://raw.githubusercontent.com/AshokDevireddy/LaunchX_team25/master/Launchx%20Chrome%20Extension/images/Enter_address_continue.png'; 
+//img1.src =  "right-arrow.png";
+document.body.appendChild(img1); 
+img1.style.width = "15%";
+img1.style.height = "20%";
+//down.innerHTML = "Image Element Added.";
+img1.style.position = "absolute"
+img1.style.top = "220px"
+img1.style.left = "330px"
+
+
+img2 = document.createElement('img'); 
+img2.src =  'https://raw.githubusercontent.com/AshokDevireddy/LaunchX_team25/master/Launchx%20Chrome%20Extension/images/Enter_email_singup.png'; 
+//img2.src =  "images/right_arrow.gif";
+document.body.appendChild(img2); 
+img2.style.width = "18%";
+img2.style.height = "20%";
+//down.innerHTML = "Image Element Added.";
+img2.style.position = "absolute"
+img2.style.top = "220px"
+img2.style.left = "950px"
