a238920c0e28e955a685ea5bd0e42c4b7d7646ca
Ashok Devireddy <53099335+AshokDevireddy@users.noreply.github.com>
2020-08-02 20:40:25 -0700

    Add files via upload

---
+++ Launchx Chrome Extension/content2.js
@@ -0,0 +1,26 @@
+a2 = document.createElement("DIV");
+a2.innerHTML = "Click Continue";
+a2.style.font = "italic bold 40px arial,serif";
+a2.style.textAlign = "center";
+a2.style.width = "20%";
+a2.style.height = "10%";
+a2.style.position = "absolute"
+a2.style.top = "250px"
+a2.style.left = "1100px"
+a2.style.backgroundColor = "yellow"
+a2.style.zIndex = 9999999;
+a2.style.opacity = 0.8;
+document.body.appendChild(a2);
+
+
+
+img2 = document.createElement('img'); 
+img2.src =  'https://www.iconsdb.com/icons/preview/black/left-xxl.png'; 
+//img2.src =  "images/right_arrow.gif";
+document.body.appendChild(img2); 
+img2.style.width = "10%";
+img2.style.height = "10%";
+//down.innerHTML = "Image Element Added.";
+img2.style.position = "absolute"
+img2.style.top = "270px"
+img2.style.left = "950px"
